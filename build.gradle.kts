14b1f5625968746e475fe4bb422e5569b59aa0d5
jakubbbbdev <veritycsgo@gmail.com>
2025-07-25 11:23:25 +0200

    Configure Gradle build system with essential setup files and IDE-specific configurations.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("io.github.goooler.shadow") version "8.1.7"
+    id("java")
+}
+
+group = "dev.jakub.github"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    implementation("org.jetbrains:annotations:24.1.0")
+    implementation("org.jetbrains:annotations:24.1.0")
+
+    implementation("org.kohsuke:github-api:1.322")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
